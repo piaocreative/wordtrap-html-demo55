1a1816689337a16d381d94acbbcd5bfd5db433ac
piaocreative <johnpiao171019@gmail.com>
2021-09-27 14:12:50 +0800

    demo55-0927

--- js/main.js
+++ js/main.js
@@ -31,9 +31,33 @@ $( document ).ready( function() {
     dots: true,
     arrows: false,
     speed: 300,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
     adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
 });
 
 $(".team-prev").click(function(e) {
